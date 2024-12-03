9acd707375a0cf26093cb011ac4949658230e25d
nandhuraju <nandhuraju6@gmail.com>
2024-12-03 22:34:04 +0530

    update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,9 +2,9 @@ import resume from '../assets/pdf/resume.pdf'
 import profile from '../assets/images/nan.png'
 
 export const headerData = {
-  name: "Nandhu Raju",
-  title: "Full Stack/Blockchain Developer",
-  desciption: "Driven by curiosity, fueled by passion, and powered by code.",
-  image: profile,
-  resumePdf: resume,
-};
+    name: 'Nandhu Raju',
+    title: "Full Stack/Blockchain Developer",
+    desciption:"Driven by curiosity, fueled by passion, and powered by code.",
+    image: profile,
+    resumePdf: resume
+}
